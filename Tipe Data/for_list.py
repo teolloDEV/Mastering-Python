d07a34bb1ea9d8fcb904c954b692152559772c15
ArN-1 <nurarif0151@gmail.com>
2024-02-22 01:18:38 +0700

    list | for

---
+++ Tipe Data/for_list.py
@@ -0,0 +1,9 @@
+list = [1, 2, 3, 4, 5, 6, 7]
+
+# for i in list:
+#     print(i)
+
+# iterasi dengan mengembalikan index dan elemen
+
+for i, j in enumerate(list):
+    print(f"Index ke - {i}, nilainya adalah : {j}")
---
+++ Tipe Data/list.py
@@ -0,0 +1,8 @@
+data = ["python", "java", "php", "c++"]
+
+#akses python dengan positif index
+print(data[0])
+#bisa juga dengan negatif index
+print(data[-4])
+#akses dengan slicing
+print(data[0:3])
